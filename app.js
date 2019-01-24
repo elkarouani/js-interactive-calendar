5daceb828a73b7487ca1331c2ae25d3e742b26fb
KDragon <elkarouani@gmail.com>
2019-01-25 00:47:58 +0100

    Task: reading events by clicking on dates

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 const inputMonth = document.getElementById("monthInput");
 const inputYear = document.getElementById("yearInput");
 const table = document.getElementById('Table');
+const eventContent = document.getElementById('eventContent');
 const tableRows = table.children[0].children;
-let monthNum = 1 ;
+let event = { 1 : {content: "Concert", year: 2019, month: 1, day: 24},
+			  2 : {content: "Theatre", year: 2019, month: 1, day: 18}
+			};
+let monthNum = 1;
 let monthIndex = 0;
 let yearNum = 2019;
 
@@ -127,10 +131,29 @@ for (let i = 0; i < 6; i++) {
 	for (let j = 0; j <= 6; j++) {
 		let cell = row.children[j];
 
-		cell.addEventListener('click', (event) => {
-			console.log((i + 1) + " and " + (j + 1));
+		cell.addEventListener('click', (e) => {
+			let day = parseInt(cell.innerHTML, 10);
+			clickedDate = new Date(yearNum, monthIndex, day);
+			
+			for (var i = 1; i <= Object.keys(event).length; i++) {
+				eventDate = new Date(event[i].year, (event[i].month - 1), event[i].day);
+				
+				equals = (date1, date2) => {
+					if (date1.getFullYear() == date2.getFullYear()) {
+						if (date1.getMonth() == date2.getMonth()) {
+							if (date1.getDate() == date2.getDate()) {
+								return true;
+							}
+						}
+					}else {
+						return false;
+					}
+				};
+
+				if (equals(clickedDate, eventDate) == true) {
+					eventContent.innerHTML = "<p>Event that day : " + event[i].content + "</p>";
+				}
+			}
 		})
 	}
 }
-
-
